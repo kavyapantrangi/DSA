e34a16a408249f0aad23e7725de454d124c811cb
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-05-25 22:12:49 +0530

    Update day4.java
    https://leetcode.com/problems/valid-anagram/

--- day4.java
+++ day4.java
@@ -6,3 +6,25 @@ Approch: concatanate the string s with again string s and check whether the  goa
         
     }
 }
+//the anagrams string problem:
+Approch:
+Anagrams:Which strings can be equal after rearranging the characters
+1.convert them into the character array.
+2.sort the arrays
+3.check whether the arrays are equal or not
+  class Solution {
+    public boolean isAnagram(String s, String t) {
+            // Your code here
+       char []ch1=s.toCharArray();
+       char []ch2=t.toCharArray();
+       if(s.length()==t.length()){
+           Arrays.sort(ch1);
+           Arrays.sort(ch2);
+           if(Arrays.equals(ch1,ch2))
+           {
+               return true;
+           }
+       }
+       return false;
+    }
+}
